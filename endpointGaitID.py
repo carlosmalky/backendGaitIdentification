656f76f6f5edbcdd5e5299d49749aed3a42fff29
Carlos Malky <akeemmalky@gmail.com>
2023-01-25 23:29:07 -0600

    recieves JSON data

--- endpointGaitID.py
+++ endpointGaitID.py
@@ -8,13 +8,19 @@ def successfulConnection():
     return "<p>Connection was established successfully!</p>"
 
 
-@app.route("/addMedia", methods=['POST'])
+@app.route("/addMedia", methods=['POST', "GET"])
 def importMedia():
     if request.method == 'POST':
-        videoTitle = request.form['videoTitle']
-        videoDuration = request.form['videoDuration']
+        videoTitle = request.json['videoTitle']
+        videoDuration = request.json['videoDuration']
+
         response = {
             "Recieved Title: ": videoTitle,
             "Duration of video: ": videoDuration
         }
+
+        print(videoDuration, videoTitle)
+
         return jsonify(response)
+    if request.method == "GET":
+        return "hola dude"
