81649740d8f650e7f07c468dd437fc15da07df30
Carlos Malky <akeemmalky@gmail.com>
2023-02-10 16:53:38 -0600

    recieves base64 correctly

--- endpointGaitID.py
+++ endpointGaitID.py
@@ -7,6 +7,33 @@ app = Flask(__name__)
 def successfulConnection():
     return "<p>Connection was established successfully!</p>"
 
+@app.route("/base64Endpoint", methods=['POST','GET'])
+def base64Endpoint():
+    if request.method == 'POST':
+        videoBase64 = request.json['videoBase64']
+        videoUri = request.json["videoUri"]
+        # print("Video Base64: ", videoBase64)
+        print("bas64 endpoint reached succesfully")
+
+        f = open("base64.txt", "w")
+        f.write(videoBase64)
+        f.close()
+
+        personIdentity = personIdentification(videoBase64)
+        return personIdentity
+
+
+@app.route("/videoEndpoint", methods=['POST','GET'])
+def videoEndpoint():
+    if request.method == 'POST':
+        videoData = request.files['video']
+        
+        response = {
+            'status': 'success',
+            'video_duration': 'video_duration'
+        }
+        print(response)
+        return response
 
 @app.route("/addMedia", methods=['POST', "GET"])
 def importMedia():
@@ -15,12 +42,22 @@ def importMedia():
         videoUri = request.json['videoUri']
 
         response = {
-            "Recieved Title: ": videoId,
-            "Duration of video: ": videoUri
+            "Video ID: ": videoId,
+            "Video URI: ": videoUri
         }
 
-        print("Video uri: ", videoUri, "Video ID: ",videoId)
+        print(response)
 
-        return jsonify(response)
+        return response
+
     if request.method == "GET":
         return "hola dude"
+
+
+def personIdentification(video):
+    response = {
+        'userFound' : 'true',
+        'userName':'Akeem',
+        'userID':"001",
+    }
+    return response
